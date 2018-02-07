88748261b56ef92f5ddf8ac8794319c349372622
hank <henrik@hankolsen.com>
2018-02-07 16:03:22 +0100

    Allow CORS in server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,13 @@ const fs = require('fs');
 
 const app = express();
 const port = process.env.PORT || 5000;
+app.all('*', (req, res, next) => {
+  const origin = req.get('origin');
+  res.header('Access-Control-Allow-Origin', origin);
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 app.get('/api/crossword', (req, res) => {
   fs.readFile('./api/crossword-data.json', { encoding: 'utf-8' }, (err, data) => {
