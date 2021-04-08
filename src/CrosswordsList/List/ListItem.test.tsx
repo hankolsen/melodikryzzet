4d4c7b56d74ce7b3a9280cd871f52f0bb1ea98bb
hank <henrik@hankolsen.com>
2021-04-09 00:59:01 +0200

    Refactor ListItem to tsx and add test

---
+++ src/CrosswordsList/List/ListItem.test.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { screen, render } from 'test/test-utils';
+import ListItem from './ListItem';
+
+describe('ListItem test', () => {
+  it('should render a list item', () => {
+    const crossword = {
+      id: 'abc-123',
+      name: 'The Crossowrd to rule them all',
+    };
+    const { container } = render(
+      <BrowserRouter>
+        <ListItem crossword={crossword} />
+      </BrowserRouter>,
+    );
+    expect(
+      screen.queryByText(/the crossowrd to rule them all/i),
+    ).toBeInTheDocument();
+    expect(container.querySelector('a')?.getAttribute('href')).toBe(
+      '/crossword/abc-123',
+    );
+  });
+});
